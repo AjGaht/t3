a004457d38688d0dd60e509ad2d185053c29cafb
AjGaht <ajgaht@protonmail.com>
2023-10-14 16:00:03 +0400

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      domain="dev-7xti7mnfym5msahq.us.auth0.com"
      clientId="tqyzHxOuZgBII146i8XKNERzEU2m5ZkG"
      authorizationParams={{
-      redirect_uri: "http://localhost:5173/"
+      redirect_uri: "http://localhost:5173"
      }}
      audience="http://localhost:8000"
      scope="openid profile email"
